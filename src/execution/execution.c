1da5a86cd5ab7064391ef8de1f07b2320bb8eabd
Alex <alejandro.ramos.gua@gmail.com>
2025-08-05 14:11:27 +0100

    started forking and creating child_processes. Need to adapt child_process function to our struct implementation

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -12,11 +12,36 @@
 
 #include "../inc/minishell.h"
 
+void	child_process(int i, t_data *all, int **pipes)
+{
+	int	j;
+
+	j = -1;
+	ft_printf("starting child\n");
+	if (i == 0)
+		first_command(i, all, pipes);
+	else
+		dup(pipes[i - 1][0], STDIN_FILENO);
+	if (i == all->info.total_proc - 1)
+		last_command(all, pipes);
+	else
+		dup2(pipes[i][1], STDOUT_FILENO);
+	while (++j < all->info.total_proc - 1)
+	{
+		close(pipes[j][0]);
+		close(pipes[j][1]);
+	}
+	close(all->redirects->);
+	close(all->redirects->);
+	execute_command(all, pipes);
+}
+
 int	pipes_init(int **pipes, t_data *all)
 {
 	int	i;
 
 	i = 0;
+	ft_printf("starting pipes_init\n");
 	pipes = malloc(all->info.total_proc);
 	if (!pipes)
 		return (ft_printf("Error: Malloc Failure\n"), 1);
@@ -43,7 +68,8 @@ static void	fork_init(t_data *all, int **pipes)
 	pid_t	pid;
 
 	i = 0;
-	while (i < all->info.total_proc - 1)
+	ft_printf("starting fork_init\n");
+	while (i < all->info.total_proc)
 	{
 		pid = fork();
 		if (pid < 0)
@@ -54,16 +80,21 @@ static void	fork_init(t_data *all, int **pipes)
 		if (i == all->info.total_proc - 1)
 			all->tokens[i].pid = pid;
 		if (pid == 0)
+		{
+			sleep(100);
+			ft_printf("insert child here\n");
 			child_process(i, all, pipes);
+		}
 		i++;
 	}
 }
 
-int	open_pipes(int **pipes, t_data *all)
+void	open_pipes(int **pipes, t_data *all)
 {
 	int	i;
 
 	i = 0;
+	ft_printf("opening pipes\n");
 	while (i < all->info.total_proc)
 	{
 		if (pipe(pipes[i]) == -1)
@@ -74,6 +105,14 @@ int	open_pipes(int **pipes, t_data *all)
 		i++;
 	}
 	fork_init(all, pipes);
+	i = 0;
+	while (i < all->info.total_proc - 1)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+		i++;
+	}
+	//return (pipes);
 }
 
 int	execution(t_data *all)
@@ -83,6 +122,8 @@ int	execution(t_data *all)
 	//if its builtin, dont exit
 	//builtin(all);
 	//if needs fork
+	ft_printf("\nStarting exec\n");
+	ft_printf("\n----------EXECUTION---------\n");
 	pipes_init(&pipes, all);
 	open_pipes(&pipes, all);
 	return (0);
