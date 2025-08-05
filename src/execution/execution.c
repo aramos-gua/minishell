f26e89da9aed6aa1ea4c22f13ed22c9cc7de2b4d
Alex <alejandro.ramos.gua@gmail.com>
2025-08-05 13:30:36 +0100

    Added function to init forks, pipes are already open theoretically

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -37,6 +37,45 @@ int	pipes_init(int **pipes, t_data *all)
 	return (0);
 }
 
+static void	fork_init(t_data *all, int **pipes)
+{
+	int		i;
+	pid_t	pid;
+
+	i = 0;
+	while (i < all->info.total_proc - 1)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			ft_printf("Error: Fork Failure\n");
+			exit(1);
+		}
+		if (i == all->info.total_proc - 1)
+			all->tokens[i].pid = pid;
+		if (pid == 0)
+			child_process(i, all, pipes);
+		i++;
+	}
+}
+
+int	open_pipes(int **pipes, t_data *all)
+{
+	int	i;
+
+	i = 0;
+	while (i < all->info.total_proc)
+	{
+		if (pipe(pipes[i]) == -1)
+		{
+			ft_printf("Error: Pipes Failure\n");
+			exit(1);
+		}
+		i++;
+	}
+	fork_init(all, pipes);
+}
+
 int	execution(t_data *all)
 {
 	int	*pipes;
@@ -45,5 +84,6 @@ int	execution(t_data *all)
 	//builtin(all);
 	//if needs fork
 	pipes_init(&pipes, all);
+	open_pipes(&pipes, all);
 	return (0);
 }
