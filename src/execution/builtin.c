3a061f4cc38c7ab2940d1424462aedb2faea6386
Alex <alejandro.ramos.gua@gmail.com>
2025-08-23 16:52:17 +0100

    cd working and updating env properly

--- src/execution/builtin.c
+++ src/execution/builtin.c
@@ -25,16 +25,43 @@ int	ft_pwd(char *cmd)
 	return (0);
 }
 
+int	get_env_index(char **arr, char *to_update, int len)
+{
+	int	i;
+
+	i = 0;
+	while (ft_strncmp(arr[i], to_update, len) != 0)
+		i++;
+	return (i);
+}
+
+int	update_env_cd(t_data *all, char *search, char *path)
+{
+	int		i;
+	char	*updated_path;
+
+	updated_path = ft_strjoin(search, path);
+	if (!updated_path)
+		return (1);
+	i = get_env_index(all->c_envp, search, ft_strlen(search));
+
+	all->c_envp[i] = updated_path;
+	return (0);
+}
+
 int	ft_cd(char *cmd, t_data *all)
 {
 	t_token	*cd_node;
 
 	dprintf(2, "running [%s]\n", cmd);
+	update_env_cd(all, "OLDPWD=", getcwd(NULL, 0));
 	cd_node = all->tokens->next;
 	dprintf(2, "token in node is [%s]\n", cd_node->token);
+	dprintf(2, "env [1] is [%s]\n", all->c_envp[0]);
 	while (ft_strncmp(cd_node->token, "cd", 2))
 		cd_node = cd_node->next;
 	chdir((const char *)cd_node->next->token);
+	update_env_cd(all, "PWD=", getcwd(NULL, 0));
 	return (0);
 }
 
@@ -50,7 +77,6 @@ int	which_builtin(char *cmd, t_data *all)
 	}
 	else if (!ft_strncmp(cmd, "cd", len))
 	{
-		//dprintf(2, "builtin [%s] not implemented\n", cmd);
 		ft_cd(cmd, all);
 		return (1);
 
