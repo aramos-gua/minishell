4be83a59d34509b54e04b41d6affe1e5b392464e
Alex <alejandro.ramos.gua@gmail.com>
2025-09-28 18:54:42 +0100

    changing things on pipes to match return values and outputs!

--- src/execution/export.c
+++ src/execution/export.c
@@ -147,7 +147,7 @@ int	ft_export(t_data *all, int proc, t_token *cmd_node)
 
 	key_val = NULL;
 	if (all->total_proc > 1)
-		return (1);
+		return (all->return_val = 1, 1);
 	if ((ft_lstsize(all->tokens, proc)) == 1)
 		ft_print_exp(all);
 	arg = cmd_node->next;
